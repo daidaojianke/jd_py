966170f53d731752cc2e65939c10d2f96a9682c7
ClassmateLin <classmatelin.site@gmail.com>
2021-06-17 00:17:14 +0800

    '更新docker脚本'

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -e "\nsource $CODE_DIR/venv/bin/activate" >> /root/.bashrc
+echo -e "\nsource /jd_scripts/venv/bin/activate" >> /root/.bashrc
 echo -e "if [ -t 1 ]; then\n    exec bash\nfi" >> /etc/profile
 echo -e "0 1 * * * python /jd_scripts/jd_sign_collection.py >> /dev/null 2>&1" >> /etc/crontab
