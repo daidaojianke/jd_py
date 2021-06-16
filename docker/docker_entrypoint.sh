91e41d12decf2b16c19661f14c549418314b4ccd
ClassmateLin <classmatelin.site@gmail.com>
2021-06-17 00:07:48 +0800

    修复dockerfile错误

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-echo -e "if [ -t 1 ]; then\n    exec zsh\nfi" >> /etc/profile
+#!/bin/sh
+echo -e "if [ -t 1 ]; then\n    exec bash\nfi" >> /etc/profile
 echo -e "0 1 * * * python /jd_scripts/jd_sign_collection.py >> /dev/null 2>&1" >> /etc/crontab
