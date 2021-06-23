fbd196cada65f0fe3b0ef3b9141ab628cc8ea10d
ClassmateLin <classmatelin.site@gmail.com>
2021-06-23 23:06:57 +0800

    '修復docker-entrypoint.sh錯誤'

--- shell/docker-entrypoint.sh
+++ shell/docker-entrypoint.sh
@@ -9,12 +9,16 @@ else
   git pull
   echo "pip install 安装最新依赖"
   pip install -r requirements.txt
+  echo "替換更新文件"
+  cp ./shell/docker-entrypoint.sh /
+  chmod a+x /docker-entrypoint.sh
   echo "重新配置定时任务..."
   crontab -r
   cat ./shell/default_crontab.sh >> /etc/crontabs/root
   cat ./config/crontab.sh >> /etc/crontabs/root
   echo "重启crond 服务..."
   pkill -9 crond
-  crond -f
+  crond -f /etc/crontabs/
+
 fi
 
