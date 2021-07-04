1be3f57a9f2bd5dbbf10f0140ba09f70418c2ffd
ClassmateLin <classmatelin.site@gmail.com>
2021-07-04 20:34:36 +0800

    '添加定时任务'

--- shell/default_crontab.sh
+++ shell/default_crontab.sh
@@ -23,5 +23,11 @@
 ### 京豆夺宝 每天8点运行一次, 并随机延时1-101秒 ###
 0 8 * * * sleep $[$RANDOM%101+1] && /usr/local/bin/python /jd_scripts/jd_bean_lottery.py >> /jd_scripts/logs/jd_bean_lottery.log 2>&1
 
+# 东东农场每三小时执行一次
+0 */3 * * * sleep $[$RANDOM%101+1] && /usr/local/bin/python /jd_scripts/jd_farm.py >> /jd_scripts/logs/jd_farm.log 2>&1
+
+# 京东金融养猪猪
+0 */3 * * * sleep $[$RANDOM%101+1] && /usr/local/bin/python /jd_scripts/jd_pet_pig.py >> /jd_scripts/logs/jd_pet_pig.log 2>&1
+
 ### 每8小时执行一次更新 ###
 0 */8 * * * /docker-entrypoint.sh  >> /dev/null  2>&1
