7eb4d186655893597c304165535d4b49537c3acc
rocke5 <rock@gmaile.com>
2021-07-31 23:01:19 +0800

    add qywx

--- utils/notify.py
+++ utils/notify.py
@@ -6,8 +6,40 @@
 # @Desc    : 通知模块
 import requests
 import telegram
-from config import TG_BOT_TOKEN, TG_USER_ID, PUSH_P_TOKEN
+from config import TG_BOT_TOKEN, TG_USER_ID, PUSH_P_TOKEN,  QYWX_AM
 from utils.console import println
+
+
+def wecom_app(title, content):
+    try:
+        if not QYWX_AM:
+            print("QYWX_AM 并未设置！！\n取消推送")
+            return
+        QYWX_AM_AY = re.split(',', QYWX_AM)
+        if 4 < len(QYWX_AM_AY) > 5:
+            print("QYWX_AM 设置错误！！\n取消推送")
+            return
+        corpid = QYWX_AM_AY[0]
+        corpsecret = QYWX_AM_AY[1]
+        touser = QYWX_AM_AY[2]
+        agentid = QYWX_AM_AY[3]
+        try:
+            media_id = QYWX_AM_AY[4]
+        except:
+            media_id = ''
+        wx = WeCom(corpid, corpsecret, agentid)
+        # 如果没有配置 media_id 默认就以 text 方式发送
+        if not media_id:
+            message = title + '\n\n' + content
+            response = wx.send_text(message, touser)
+        else:
+            response = wx.send_mpnews(title, content, media_id, touser)
+        if response == 'ok':
+            print('推送成功！')
+        else:
+            print('推送失败！错误信息如下：\n', response)
+    except Exception as e:
+        print(e)
 
 
 def push_plus_notify(title, content):
